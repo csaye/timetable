9885ad44059c3d406e1a0282854c43cd1a1b3827
Cooper Saye <coopersaye@gmail.com>
2021-10-21 22:19:46 -0700

    Create homepage.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
+import Image from 'next/image';
+
+import { signInWithGoogle } from '../util/signInWithGoogle';
+
 import styles from '../styles/Index.module.css';
 
 export default function Index() {
   return (
     <div>
+      <div>
+        <Image
+          src="/img/logo.png"
+          width="48"
+          height="48"
+        />
+        <h1>Timetable</h1>
+        <button onClick={signInWithGoogle}>
+          Sign in with Google
+        </button>
+      </div>
     </div>
   );
 }
