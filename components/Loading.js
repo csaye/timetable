a3d2a44132ec5f1b45be82a7c003187ab8978e1a
Cooper Saye <coopersaye@gmail.com>
2021-10-21 22:28:40 -0700

    Improve Loading component styling.

--- components/Loading.js
+++ components/Loading.js
@@ -1,8 +1,10 @@
 import CircularProgress from '@mui/material/CircularProgress';
 
+import styles from '../styles/components/Loading.module.css';
+
 export default function Loading() {
   return (
-    <div>
+    <div className={styles.container}>
       <CircularProgress />
     </div>
   );
