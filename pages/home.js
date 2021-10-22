693275e7198d4d00c3a2d6e41542d63bddf4cb7d
Cooper Saye <coopersaye@gmail.com>
2021-10-21 22:56:23 -0700

    Route home page based on auth.

--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,21 @@
+import Loading from '../components/Loading';
+import Router from 'next/router';
+
+import { useEffect } from 'react';
+
 import styles from '../styles/pages/Home.module.css';
 
-export default function Home() {
+export default function Home(props) {
+  const { authed } = props;
+
+  // listen for routing
+  useEffect(() => {
+    if (authed === false) Router.push('/');
+  }, [authed]);
+
+  // return if loading
+  if (!authed) return <Loading />;
+
   return (
     <div>
     </div>
