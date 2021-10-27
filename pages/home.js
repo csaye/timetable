7dc891ce226adb5ccb180685218e631098122e4a
Cooper Saye <coopersaye@gmail.com>
2021-10-26 23:17:45 -0700

    Create day box fillers.

--- pages/home.js
+++ pages/home.js
@@ -1,17 +1,29 @@
 import Loading from '../components/Loading';
 import Router from 'next/router';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getAuth, signOut } from 'firebase/auth';
 
 import styles from '../styles/pages/Home.module.css';
 
+const now = new Date();
 const dayNames = [
   'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
 ];
 
 export default function Home(props) {
   const { authed } = props;
+
+  const [year, setYear] = useState(now.getFullYear());
+  const [month, setMonth] = useState(now.getMonth());
+
+  // returns the offset of the current month
+  function monthOffset() {
+    const firstOfMonth = new Date(year, month, 1);
+    return firstOfMonth.getDay();
+  }
+
+  const fillers = Array(monthOffset()).fill(0);
 
   const auth = getAuth();
 
@@ -34,6 +46,11 @@ export default function Home(props) {
             <div key={i}>
               <h3>{dayName}</h3>
             </div>
+          )
+        }
+        {
+          fillers.map((filler, i) =>
+            <span key={i} />
           )
         }
       </div>
