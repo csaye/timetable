0085bd593444e50121fc4bdb49a8a551ab7c3c03
Cooper Saye <coopersaye@gmail.com>
2021-10-28 22:35:15 -0700

    Display todos on days.

--- components/Day.js
+++ components/Day.js
@@ -1,9 +1,21 @@
 import styles from '../styles/components/Day.module.css';
 
 export default function Day(props) {
+  const { day, todos } = props;
+
   return (
-    <div className={`daybox ${styles.container}`}>
-      {props.day}
+    <div className="daybox">
+      <h1>{day}</h1>
+      <div>
+        {
+          todos &&
+          todos.map(todo =>
+            <div key={todo.id}>
+              {todo.title}
+            </div>
+          )
+        }
+      </div>
     </div>
   );
 }
