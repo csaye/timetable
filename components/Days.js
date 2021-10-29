5240eb53bc3aa6ef75ffadc88be6f04b5d077dd5
Cooper Saye <coopersaye@gmail.com>
2021-10-28 22:34:33 -0700

    Pass todos to days.

--- components/Days.js
+++ components/Days.js
@@ -1,7 +1,20 @@
 import Day from './Day';
 
+import { getAuth } from 'firebase/auth';
+import { getFirestore, collection, query, where } from 'firebase/firestore';
+import { useCollectionData } from 'react-firebase9-hooks/firestore';
+
 export default function Days(props) {
   const { month, year } = props;
+
+  const auth = getAuth();
+  const db = getFirestore();
+
+  // get todos
+  const uid = auth.currentUser.uid;
+  const todosRef = collection(db, 'todos');
+  const todosQuery = query(todosRef, where('uid', '==', uid));
+  const [todos] = useCollectionData(todosQuery, { idField: 'id' });
 
   // returns number of days in the current month
   function daysInMonth() {
@@ -12,9 +25,18 @@ export default function Days(props) {
 
   const days = Array(daysInMonth()).fill(0).map((val, i) => i + 1);
 
+  // returns todos for given day
+  function getTodos(day) {
+    return todos.filter(todo => {
+      const [y, m, d] = todo.date.split('-').map(val => parseInt(val));
+      return y === year && m - 1 === month && d === day;
+    });
+  }
+
   return (
     days.map((day, i) =>
       <Day
+        todos={todos ? getTodos(day) : undefined}
         day={day}
         month={month}
         year={year}
