00789fd47b45863bf11dafdd9f088c1996d6dc9d
Cooper Saye <coopersaye@gmail.com>
2021-10-28 21:47:12 -0700

    Extract todo creation.

--- components/Todos.js
+++ components/Todos.js
@@ -1,10 +1,19 @@
+import Modal from './Modal';
+
 import { getAuth } from 'firebase/auth';
-import { getFirestore, collection, query, where } from 'firebase/firestore';
+import {
+  getFirestore, collection, query, where, addDoc
+} from 'firebase/firestore';
 import { useCollectionData } from 'react-firebase9-hooks/firestore';
+import { useState } from 'react';
 
 export default function Todos() {
   const auth = getAuth();
   const db = getFirestore();
+
+  const [modalOpen, setModalOpen] = useState(false);
+  const [title, setTitle] = useState('');
+  const [date, setDate] = useState('');
 
   // get todos
   const uid = auth.currentUser.uid;
@@ -12,8 +21,27 @@ export default function Todos() {
   const todosQuery = query(todosRef, where('uid', '==', uid));
   const [todos] = useCollectionData(todosQuery, { idField: 'id' });
 
+  // creates new todo in firebase
+  async function createTodo() {
+    await addDoc(todosRef, {
+      title: title,
+      date: new Date(date).getTime(),
+      uid: auth.currentUser.uid
+    });
+  }
+
+  // resets modal
+  function resetModal() {
+    setTitle('');
+    setDate('');
+  }
+
   return (
     <div>
+      <button onClick={() => {
+        resetModal();
+        setModalOpen(true);
+      }}>+</button>
       {
         todos &&
         todos.map(todo =>
@@ -22,6 +50,30 @@ export default function Todos() {
           </div>
         )
       }
+      <Modal open={modalOpen} setOpen={setModalOpen}>
+        <h1>New Todo</h1>
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            createTodo();
+            setModalOpen(false);
+          }}
+        >
+          <input
+            placeholder="title"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            required
+          />
+          <input
+            type="date"
+            value={date}
+            onChange={e => setDate(e.target.value)}
+            required
+          />
+          <button>Create</button>
+        </form>
+      </Modal>
     </div>
   );
 }
