8f5e7f83b4e98d538a495ba5eb4e724ca18f6cb6
Cooper Saye <coopersaye@gmail.com>
2021-10-31 15:28:50 -0700

    Display todo time left.

--- components/Todo.js
+++ components/Todo.js
@@ -1,10 +1,24 @@
 import styles from '../styles/components/Todo.module.css';
 
+import { useEffect, useState } from 'react';
+
 export default function Todo(props) {
+  const [timeLeft, setTimeLeft] = useState(new Date(props.date) - new Date());
+
+  // update time left every tenth of a second
+  useEffect(() => {
+    setTimeLeft(new Date(props.date) - new Date());
+    const interval = setInterval(() => {
+      setTimeLeft(new Date(props.date) - new Date());
+    }, 100);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className={styles.container}>
       <h1>{props.title}</h1>
       <p>{props.date}</p>
+      {timeLeft}
     </div>
   );
 }
