348d01b8ae8ffdcf2349783e5fd41db3bc419523
Cooper Saye <coopersaye@gmail.com>
2021-10-31 16:33:29 -0700

    Display time left increments.

--- components/Todo.js
+++ components/Todo.js
@@ -2,23 +2,41 @@ import styles from '../styles/components/Todo.module.css';
 
 import { useEffect, useState } from 'react';
 
+const sec = 1000;
+const min = sec * 60;
+const hour = min * 60;
+const day = hour * 24;
+
 export default function Todo(props) {
-  const [timeLeft, setTimeLeft] = useState(new Date(props.date) - new Date());
+  const end = new Date(props.date.replaceAll('-', '/'));
+
+  const [timeLeft, setTimeLeft] = useState(end - new Date());
 
   // update time left every tenth of a second
   useEffect(() => {
-    setTimeLeft(new Date(props.date) - new Date());
+    setTimeLeft(end - new Date());
     const interval = setInterval(() => {
-      setTimeLeft(new Date(props.date) - new Date());
+      setTimeLeft(end - new Date());
     }, 100);
     return () => clearInterval(interval);
   }, []);
+
+  function Countdown() {
+    return (
+      <>
+        {timeLeft > day && <>{Math.floor(timeLeft / day)}<span>d</span></>}
+        {timeLeft > hour && <>{Math.floor(timeLeft % day / hour)}<span>h</span></>}
+        {timeLeft > min && <>{Math.floor(timeLeft % day % hour / min)}<span>m</span></>}
+        {Math.floor(timeLeft % day % hour % min / sec)}<span>s</span>
+      </>
+    );
+  }
 
   return (
     <div className={styles.container}>
       <h1>{props.title}</h1>
       <p>{props.date}</p>
-      {timeLeft}
+      <Countdown />
     </div>
   );
 }
