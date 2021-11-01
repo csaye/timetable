c20ca5548d4da4a567c78b51bd8409ce3b097d69
Cooper Saye <coopersaye@gmail.com>
2021-10-31 17:33:05 -0700

    Implement todo deletion within day.

--- components/Day.js
+++ components/Day.js
@@ -1,3 +1,7 @@
+import DeleteIcon from '@mui/icons-material/Delete';
+
+import { getFirestore, doc, deleteDoc } from 'firebase/firestore';
+
 import styles from '../styles/components/Day.module.css';
 
 const now = new Date();
@@ -5,7 +9,16 @@ const now = new Date();
 export default function Day(props) {
   const { day, todos } = props;
 
+  const db = getFirestore();
+
   const today = day === now.getDate();
+
+  // deletes given todo from firebase
+  async function deleteTodo(todo) {
+    if (!window.confirm(`Delete ${todo.title}?`)) return;
+    const todoRef = doc(db, 'todos', todo.id);
+    await deleteDoc(todoRef);
+  }
 
   return (
     <div className={
@@ -18,6 +31,9 @@ export default function Day(props) {
           todos.map(todo =>
             <div key={todo.id}>
               {todo.title}
+              <button onClick={() => deleteTodo(todo)}>
+                <DeleteIcon />
+              </button>
             </div>
           )
         }
