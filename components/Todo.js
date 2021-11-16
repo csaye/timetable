b85ea0481357b72f4c466e529d05958e817e4142
Cooper Saye <coopersaye@gmail.com>
2021-11-16 11:17:58 -0800

    Integrate todo modal.

--- components/Todo.js
+++ components/Todo.js
@@ -1,11 +1,9 @@
-import Modal from './Modal';
+import TodoModal from './TodoModal';
 import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
 
 import styles from '../styles/components/Todo.module.css';
 
 import { useEffect, useState } from 'react';
-import { getFirestore, doc, deleteDoc, updateDoc } from 'firebase/firestore';
 
 // time intervals
 const sec = 1000;
@@ -21,37 +19,12 @@ export default function Todo(props) {
   );
 
   const [modalOpen, setModalOpen] = useState(false);
-  const [newTitle, setNewTitle] = useState(title);
-  const [newDate, setNewDate] = useState(date);
-
-  const db = getFirestore();
-  const todoRef = doc(db, 'todos', id);
 
   // // update time left when date changes
   useEffect(() => {
     const end = new Date(date.replaceAll('-', '/'));
     setTimeLeft(end - new Date());
   }, [date]);
-
-  // deletes current todo from firebase
-  async function deleteTodo() {
-    if (!window.confirm(`Delete ${title}?`)) return;
-    await deleteDoc(todoRef);
-  }
-
-  // updates todo in firebase
-  async function updateTodo() {
-    setModalOpen(false);
-    await updateDoc(todoRef, {
-      title: newTitle,
-      date: newDate
-    });
-  }
-
-  // resets modal values
-  function resetModal() {
-    setNewTitle(title);
-  }
 
   function Countdown() {
     return (
@@ -73,38 +46,15 @@ export default function Todo(props) {
       <Countdown />
       <button
         className={styles.editbutton}
-        onClick={() => {
-          resetModal();
-          setModalOpen(true);
-        }}
+        onClick={() => setModalOpen(true)}
       >
         <EditIcon />
       </button>
-      <Modal open={modalOpen} setOpen={setModalOpen}>
-        <h1>Editing Todo</h1>
-        <form onSubmit={e => {
-          e.preventDefault();
-          updateTodo();
-        }}>
-          <input
-            value={newTitle}
-            onChange={e => setNewTitle(e.target.value)}
-            required
-          />
-          <input
-            type="date"
-            value={newDate}
-            onChange={e => setNewDate(e.target.value)}
-            required
-          />
-          <button className="graybutton">
-            Update
-          </button>
-        </form>
-        <button className="iconbutton" onClick={deleteTodo}>
-          <DeleteIcon />
-        </button>
-      </Modal>
+      <TodoModal
+        todo={props}
+        modalOpen={modalOpen}
+        setModalOpen={setModalOpen}
+      />
     </div>
   );
 }
